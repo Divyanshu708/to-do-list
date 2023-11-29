6a0b66e888fe2655e3c54872c931228943e72743
Divyanshu MJ <141554205+Divyanshu708@users.noreply.github.com>
2023-11-29 18:49:03 +0530

    Inital commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ToDoList />
     </div>
   );
 }
+
+function ToDoList() {
+  return (
+    <div>
+      <h1>To Do List</h1>
+    </div>
+  );
+}
+
+function SearchBox() {}
+
+function Tasks() {}
+
+function OptionBar() {}
+
+function AddTaskModel() {}
 
 export default App;
