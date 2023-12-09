ca27899b72c54bd9fd73b1559a17c5c3e4b516f0
Divyanshu MJ <141554205+Divyanshu708@users.noreply.github.com>
2023-12-09 23:16:12 +0530

    Normal Interface is completed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import "./App.css";
+const demoTask = [
+  { id: 8939982739871028, task: "Wake up", time: "8:00", active: false },
+  { id: 8925285465654578, task: "Eat Breakfast", time: "10:00", active: false },
+  { id: 8123558465259647, task: "Go to Work", time: "11:00", active: false },
+  { id: 8121235534245964, task: "Do work", time: "11:00", active: false },
+];
 
 function App() {
   return (
-    <div>
+    <div className="WholeBody">
       <ToDoList />
     </div>
   );
@@ -11,17 +16,84 @@ function App() {
 function ToDoList() {
   return (
     <div>
-      <h1>To Do List</h1>
+      <h1 style={{ textAlign: "center" }}>To Do List</h1>
+      <div>
+        <SearchBox />
+        <br />
+        <TasksList />
+        <br />
+        <OptionBar />
+      </div>
     </div>
   );
 }
 
-function SearchBox() {}
+function SearchBox() {
+  return (
+    <div>
+      <form>
+        <input
+          type="text"
+          placeholder="Search Tasks"
+          style={{ width: "25rem", height: "2.5rem" }}
+        ></input>
+        {"   "}
+        <input
+          style={{ width: "3rem", height: "2.9rem" }}
+          type="Submit"
+          value="🔎"
+        ></input>
+      </form>
+    </div>
+  );
+}
 
-function Tasks() {}
+function TasksList() {
+  return (
+    <div className="Lists">
+      {demoTask.map((res) => (
+        <Tasks data={res} key={res.id} />
+      ))}
+    </div>
+  );
+}
 
-function OptionBar() {}
+function Tasks({ data }) {
+  return (
+    <>
+      <div className="Tasks">
+        <input
+          type="checkbox"
+          style={{ display: "inline", marginLeft: "1rem" }}
+        ></input>
+        <p style={{ display: "inline" }}>
+          <span style={{ marginLeft: "1rem", fontSize: "20px" }}>
+            {data.task}
+          </span>{" "}
+          <span
+            style={{ float: "right", marginRight: "1rem", fontSize: "20px" }}
+          >
+            {data.time}
+          </span>
+        </p>
+      </div>
+    </>
+  );
+}
 
-function AddTaskModel() {}
+function OptionBar() {
+  return (
+    <div className="OptionBar">
+      <button>Add Task</button>
+      <div style={{ display: "inline" }} className="rightNav">
+        <button>{" All "}</button> <button>Active</button>
+        {"  "}
+        <button className="activeBtn">Completed</button>
+      </div>
+    </div>
+  );
+}
+
+// function AddTaskModel() {}
 
 export default App;
